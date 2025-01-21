d547dc87bc89652a9f6e2b256bb83f0c26143bcb
Domenicobrz <domenicobrz@gmail.com>
2025-01-21 16:01:42 +0100

    included comment

--- src/lib/shaders/integrators/ReSTIRPTShader.ts
+++ src/lib/shaders/integrators/ReSTIRPTShader.ts
@@ -141,6 +141,10 @@ struct PathInfo {
 
 struct Reservoir {
   Y: PathInfo,
+  // this will be used to make sure the path-shift selects the correct first bounce
+  // remember that after the first SR reuse, we may end up using a seed that is different
+  // from the seed that generated the first bounce hit. And the pixel-shift always have to land
+  // on the original first bounce hit to be useable in the Generalized Balance Heuristic
   domain: vec2i,
   Gbuffer: vec4f, // normal.xyz, depth at first bounce. depth = -1 if no intersection was found
   Wy: f32,  // probability chain
